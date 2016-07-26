4140bd0ad619b869aa7c9259bba11909f8178724
Danilo <danilonewtrue@gmail.com>
2016-07-26 20:08:17 -0300

    Prototype Object

---
+++ prototype.js
@@ -0,0 +1,19 @@
+/* Prototype - Herança em Javascript */
+
+var Animal = function() {
+
+}
+
+Animal.prototype.name = '';
+Animal.prototype.type = '';
+
+Animal.prototype.move = function() {
+
+}
+Animal.prototype.say = function() {
+
+}
+
+console.log(new Animal);
+
+/* Para que serve o objeto prototype? */
